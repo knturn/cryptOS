d4e63691a41c04dca1c7509c54a38fd074a5fab8
Kaan Turan <kaann.dev@gmail.com>
2023-02-23 14:32:55 +0300

    Initial Commit

---
+++ cryptOS/cryptOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptOSApp.swift
+//  cryptOS
+//
+//  Created by Turan, Kaan on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct cryptOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
